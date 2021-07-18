e24adcaac9eacff97e3a516bbe35762a0aae686d
abidsulaiman <abidqubit@gmail.com>
2021-07-18 21:10:35 +0530

    files added

---
+++ src/request.js
@@ -0,0 +1,17 @@
+// https://api.themoviedb.org/3/movie/550?api_key=309cc732ca43ec0ceec4a8dabd774c2e
+
+// 309cc732ca43ec0ceec4a8dabd774c2e
+const APIKEY = "309cc732ca43ec0ceec4a8dabd774c2e";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${APIKEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${APIKEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${APIKEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${APIKEY}&with_genres=99`,
+}
+
+export default requests;
